34f367cc44a072f9c43edaffbdc369281d61bdd9
looombook <146741550+looombook@users.noreply.github.com>
2023-10-04 18:29:37 +0800

    代码测试

---
+++ qian/src/components/Aside.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+  <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
+    <el-radio-button :label="false">expand</el-radio-button>
+    <el-radio-button :label="true">collapse</el-radio-button>
+  </el-radio-group>
+  <el-menu
+      style="width:200px; min-height:calc(100vh - 50px)"
+      default-active="2"
+      class="el-menu-vertical-demo"
+      :collapse="isCollapse"
+  >
+    <el-sub-menu index="1">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>Navigator One</span>
+      </template>
+    </el-sub-menu>
+    <el-menu-item index="2">
+      <el-icon><icon-menu /></el-icon>
+      <template #title>Navigator Two</template>
+    </el-menu-item>
+    <el-menu-item index="3" disabled>
+      <el-icon><document /></el-icon>
+      <template #title>Navigator Three</template>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <el-icon><setting /></el-icon>
+      <template #title>Navigator Four</template>
+    </el-menu-item>
+  </el-menu>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: `Aside`
+}
+</script>
+
+<style scoped>
+
+</style>
