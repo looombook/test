22f19d336049542dc74f0a437835c24a8963fe83
looombook <146741550+looombook@users.noreply.github.com>
2023-10-04 18:53:12 +0800

    代码测试

---
+++ components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="height:   50px; line-height: 50px;border-bottom:  1px solid #ccc;display: flex">
+    <div style="weith:200px;padding-left: 30px;font-weight: bold;color: blue">后台管理</div>
+    <div style="flex:1"></div>
+    <div style="width:100px">
+    <el-dropdown style="display: contents">
+    我的<span style="margin-top:20px" class="el-dropdown-link" >
+      Dropdown List
+      <el-icon class="el-icon--right">
+        <arrow-down />
+      </el-icon>
+    </span>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>个人信息</el-dropdown-item>
+          <el-dropdown-item>退出系统</el-dropdown-item>
+
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: `Header`,
+}
+</script>
+
+<style scoped>
+
+</style>
